576ab43062f973f9f4ea75f087c6bc8118489604
huascargutierrez2006 <huascar.gutierrez.m@ucb.edu.bo>
2026-04-26 17:13:54 -0400

    Añadida funcionalidad para listar materias

--- main.cpp
+++ main.cpp
@@ -35,7 +35,30 @@ void registrarEstudiante(sqlite3* db) {
     sqlite3_exec(db, sql.c_str(), 0, 0, 0);
     cout << "Registrado!" << endl;
 }
+void registrarMateria(sqlite3* db) {
+    string nombreMateria;
+    cout << "\n--- Registrar Nueva Materia ---" << endl;
+    cout << "Nombre de la materia: ";
+    cin.ignore(); // Limpia el buffer del teclado
+    getline(cin, nombreMateria); // Permite nombres con espacios como "Algebra Lineal"
 
+    string sql = "INSERT INTO Materias (nombre_materia) VALUES ('" + nombreMateria + "');";
+
+    char* mensajeError = 0;
+    int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &mensajeError);
+
+    if (rc != SQLITE_OK) {
+        cerr << "Error: " << mensajeError << endl;
+        sqlite3_free(mensajeError);
+    } else {
+        cout << "Materia guardada con éxito." << endl;
+    }
+}
+void listarMaterias(sqlite3* db) {
+    cout << "\n--- Catalogo de Materias ---" << endl;
+    const char* sql = "SELECT * FROM Materias;";
+    sqlite3_exec(db, sql, imprimirFila, 0, 0);
+}
 // 3. FUNCIÓN PRINCIPAL (El corazón del programa)
 int main() {
     sqlite3* db;
@@ -43,12 +66,21 @@ int main() {
 
     int opcion;
     do {
-        cout << "\n1. Registrar\n2. Listar\n0. Salir\nOpcion: ";
+        cout << "\n==== MENU PRINCIPAL ====" << endl;
+        cout << "1. Registrar Estudiante" << endl;
+        cout << "2. Ver Estudiantes" << endl;
+        cout << "3. Registrar Materia" << endl;
+        cout << "4. Ver Materias" << endl;
+        cout << "0. Salir" << endl;
+        cout << "Opcion: ";
         cin >> opcion;
 
-        if (opcion == 1) registrarEstudiante(db);
-        else if (opcion == 2) listarEstudiantes(db);
-
+        switch(opcion) {
+            case 1: registrarEstudiante(db); break;
+            case 2: listarEstudiantes(db); break;
+            case 3: registrarMateria(db); break;
+            case 4: listarMaterias(db); break;
+        }
     } while (opcion != 0);
 
     sqlite3_close(db);
